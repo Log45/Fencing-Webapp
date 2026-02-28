c5d41374a95cd15541912df87da67e30a2e0efb4
Logan Endes <log@csh.rit.edu>
2026-02-28 03:10:44 -0500

    refactor ml api and add Spring Boot template project

---
+++ backend/src/main/java/csh/log/fencingreferee/FencingrefereeApplication.java
@@ -0,0 +1,13 @@
+package csh.log.fencingreferee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FencingrefereeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FencingrefereeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/csh/log/fencingreferee/FencingrefereeApplicationTests.java
@@ -0,0 +1,13 @@
+package csh.log.fencingreferee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FencingrefereeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
