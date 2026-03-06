3c647706fed43511004c399f9ed9657ef24b66c8
Logan Endes <log@csh.rit.edu>
2026-03-06 11:49:40 -0500

    bug: fix CORS allowed origins so we can add trusted routes in deployment

--- backend/src/main/java/csh/log/fencingreferee/config/WebConfig.java
+++ backend/src/main/java/csh/log/fencingreferee/config/WebConfig.java
@@ -1,20 +1,22 @@
 package csh.log.fencingreferee.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    private final String[] allowedOrigins;
+
+    public WebConfig(@Value("${app.cors.allowed-origins}") String allowedOrigins) {
+        this.allowedOrigins = allowedOrigins.split(",");
+    }
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-            .allowedOrigins("http://localhost:5173")
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-            .allowedHeaders("*");
-        registry.addMapping("/api/**")
-            .allowedOrigins("http://localhost:3000")
+            .allowedOrigins(allowedOrigins)
             .allowedMethods("*")
             .allowedHeaders("*")
             .allowCredentials(true);
